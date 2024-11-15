f05062a0f71d8f89dcb3038e4cc05074fc2b1300
Cao Đình Trung <91171043+CaoTrungCa@users.noreply.github.com>
2024-11-16 00:07:31 +0700

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="pyro">
+          <div className="before"></div>
+          <div className="after"></div>
+        </div>
+        {children}
+      </body>
     </html>
   )
 }
